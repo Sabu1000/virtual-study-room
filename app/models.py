11ee4b81b15ee0cd37a5fe16ad9e0a0a4269cae4
Sabu Bhattarai <bhattaraisabu@gmail.com>
2025-06-19 11:57:05 -0500

    Step 1: Add bio and profile image fields to User model

--- app/models.py
+++ app/models.py
@@ -9,6 +9,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(25), nullable=False, unique=True) # all field must have value and must be unique. can have 25 char max
     email = db.Column(db.String(120), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
+    # new profile 
+    bio = db.Column(db.Text, nullable=True) 
+    image_file = db.Column(db.String(120), default="default.jpg")
+    
+    study_rooms = db.relationship('StudyRoom', backref='host', lazy=True)
+    messages = db.relationship('Message', backref='user', lazy=True)
+
+
     
 
 class StudyRoom(db.Model):
